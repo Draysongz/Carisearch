b33e613f82d5b4b903785573506cb5103cb91665
ackerman <dharmiegra4@gmail.com>
2022-08-15 22:13:45 +0100

    updated

--- js/script.js
+++ js/script.js
@@ -30,15 +30,26 @@ let searchBtn = document.querySelector('#search')
 
 searchBtn.addEventListener('click', (e)=>{
 e.preventDefault()
+let result = document.getElementById('result')
 
 var query= document.querySelector('#searchbar').value
 var API_KEY ='5f8d2545199a43617f85ba63acf7450a' 
-var url= 'https://api.serpstack.com/search?access_key=' + API_KEY +"&type=web&query=" + query
-console.log(url);
+var url= 'http://api.serpstack.com/search?access_key=' + API_KEY +"&type=web&query=" + query
+console.log(url)
 
-
-fetch(url, function(data){
+fetch(url).then((data)=>{
     console.log(data);
-}) 
+    let organic = data.json()
+    console.log(organic.PromiseResult);
+//     organic.forEach(res => {
+//         result = `
+//          <h1>${res.title}</h1><br><a href='${res.url}'>${res.url}</a>
+//          <p>${res.snippet}</p>;
+         
+         
+//          `
+//          document.getElementById('result').append(result)
+//     });
+// }) 
 
-})
+})})
